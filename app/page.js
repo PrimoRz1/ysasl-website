c4c9e9f8f509b3d6b95540615648740e4ae67f02
PrimoRz1 <recendez18@gmail.com>
2026-09-16 23:55:33 -0700

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Yuba Sutter Adult Soccer League</h1>
+      <h2>YSASL</h2>
+      <p>Sitio oficial de la liga.</p>
+
+      <p>
+        Primera División • Segunda División
+      </p>
+    </main>
+  );
+}
